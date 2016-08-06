e193f70062fa01a2c5b2346f53dac7525289e86e
Mike Poirier <mikepoirier26@gmail.com>
2016-08-06 10:40:02 -0600

    Add Foundation and jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
-    },
-    resolve: {
-        root: __dirname,
-        alias: {},
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015', 'stage-0']
-                },
-                test: /\.jsx?$/,
-                exclude: /(node_moudles|bower_components)/
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map'
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {},
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_moudles|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
