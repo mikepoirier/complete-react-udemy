34fdc31ccbe859666e2142be47d547b1ec2c1840
Mike Poirier <mikepoirier26@gmail.com>
2016-08-06 19:44:21 -0600

    Fix modal bug. Refactor Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,17 +1,23 @@
 var React = require('react');
 var {Link} = require('react-router');
 
+const getLocationPath = (location) =>
+  `/?location=${encodeURIComponent(location)}`;
+
 var Examples = (props) => {
+  var denver = 'Denver, CO';
+  var rio = 'Rio, Brazil';
+
   return (
     <div>
       <h1 className="text-center page-title">Examples</h1>
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Denver'>Denver, CO</Link>
+          <Link to={getLocationPath(denver)}>{denver}</Link>
         </li>
         <li>
-          <Link to='/?location=Rio'>Rio, Brazil</Link>
+          <Link to={getLocationPath(rio)}>{rio}</Link>
         </li>
       </ol>
     </div>
